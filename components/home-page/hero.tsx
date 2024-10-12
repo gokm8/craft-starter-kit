3403beb5663063d89f04767ee93a19f0fb45e15b
gokm8 <gouzb22@student.sdu.dk>
2024-10-12 20:23:52 +0200

    add components and overall structure

---
+++ components/home-page/hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Section, Container } from "@/components/craft";
+
+export default function Hero() {
+    return (
+        <Section>
+            <Container>
+                <h1>Hero</h1>
+                <p>- Hero -</p>
+            </Container>
+        </Section>
+    );
+}
---
+++ components/home-page/testimonials.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Section, Container } from "@/components/craft";
+
+export default function Testimonials() {
+    return (
+        <Section>
+            <Container>
+                <h1>Testimonials</h1>
+                <p>- Testimonial -</p>
+            </Container>
+        </Section>
+    );
+}
